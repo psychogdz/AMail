cb2ebb0b8629330e9ee3755d4a4d662d492152d1
psychogdz <p11650011@gmail.com>
2026-09-02 15:53:27 +0330

    Allow test suite execution in production environment by conditionally disabling SSL redirect for test runner

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from .base import *
 
 DEBUG = False
@@ -18,6 +19,15 @@ SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_HTTPONLY = False  # Allows JS CSRF token reading if needed
 SESSION_COOKIE_SAMESITE = 'Lax'
 CSRF_COOKIE_SAMESITE = 'Lax'
+
+# Testing overrides (enables running `manage.py test` in production environment)
+TESTING = 'test' in sys.argv or any('test' in arg for arg in sys.argv)
+if TESTING:
+    SECURE_SSL_REDIRECT = False
+    SECURE_HSTS_SECONDS = 0
+    ALLOWED_HOSTS = ['*']
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
 
 # HTTP Security Headers
 SECURE_BROWSER_XSS_FILTER = True
